58f9f30723ed05bbc2078319b62b19218bdf1115
divyakesharwani <dkesharw@chicagobooth.edu>
2022-03-12 17:46:31 +0000

    follow request resource created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_172418) do
+ActiveRecord::Schema.define(version: 2022_03_12_172934) do
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
